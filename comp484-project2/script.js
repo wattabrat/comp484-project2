551add1f0532de69ad6a53638610b7adfef66bae
Violeta Barajas <inaquarelle@icloud.com>
2026-04-14 22:28:05 -0700

    inital commit

---
+++ comp484-project2/script.js
@@ -0,0 +1,162 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Timer } from 'three/addons/misc/Timer.js';
+
+let scene, camera, renderer, pikachuModel, mixer;
+let timer = new Timer();
+let animations = {};
+let currentAction;
+
+function init3D() {
+
+  //window size
+  const SIZE = 500;
+
+  // set up scene
+  scene = new THREE.Scene();
+
+ //set up camera
+  camera = new THREE.PerspectiveCamera(70, 1, 0.1, 1000);
+  camera.position.z = 2;
+  camera.position.y = 1;
+  camera.lookAt(0, 1, 0);
+
+  //renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(SIZE, SIZE);
+  document.getElementById('pet-canvas-container').appendChild(renderer.domElement);
+
+
+  //lights
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  scene.add(directionalLight);
+  directionalLight.position.set(0, 1, 1).normalize();
+
+  const ambientLight = new THREE.AmbientLight(0xeac8ca, 2);
+  scene.add(ambientLight);
+  scene.background = new THREE.Color(0xeac8ca);
+
+
+  //load model (GLB format)
+  const loader = new GLTFLoader();
+  loader.load('pikachu/source/pikachu2.glb', (gltf) => {
+    pikachuModel = gltf.scene;
+    scene.add(pikachuModel);
+
+    mixer = new THREE.AnimationMixer(pikachuModel);
+
+    animations = {
+      walk: mixer.clipAction(gltf.animations[0]),
+      dance: mixer.clipAction(gltf.animations[1]),
+      jump: mixer.clipAction(gltf.animations[2]),
+      idle: mixer.clipAction(gltf.animations[3])
+    };
+
+    currentAction = animations.idle;
+    currentAction.play();
+
+   /* if (gltf.animations && gltf.animations.length > 0) {
+      mixer = new THREE.AnimationMixer(pikachuModel);
+      mixer.clipAction(gltf.animations[3]).play();
+      //0 walk
+      //1 dance
+      //2 jump
+      //3 idle
+    }*/
+
+  });
+
+  animate();
+
+}
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  timer.update();
+
+  if (mixer) {
+    mixer.update(timer.getDelta());
+  }
+  renderer.render(scene, camera);
+}
+
+function switchAnimation(newAction)
+{
+  if (currentAction === newAction) return;
+
+   currentAction.fadeOut(0.5);
+   newAction.reset().fadeIn(0.5).play();
+
+   //update tracker of current action
+   currentAction = newAction;
+}
+
+
+
+//variable to hold pet information (name, weight, happiness)
+var pikachu_info = {name:"Pikachu", weight:10, happiness:5};
+
+
+
+$(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
+
+init3D();
+
+    // Called function to update the name, happiness, and weight of our pet in our HTML
+    checkAndUpdatePetInfoInHtml();
+  
+    // When each button is clicked, it will "call" function for that button (functions are below)
+    $('.treat-button').click(clickedTreatButton);
+    $('.play-button').click(clickedPlayButton);
+    $('.exercise-button').click(clickedExerciseButton);
+
+  });
+  
+  
+    function clickedTreatButton() {
+      // Increase pet happiness
+      pikachu_info['happiness'] += 1;
+      // Increase pet weight
+      pikachu_info['weight'] += 1;
+      switchAnimation(animations.jump.setLoop(THREE.LoopRepeat, 2));
+      checkAndUpdatePetInfoInHtml();
+    }
+    
+    function clickedPlayButton() {
+      // Increase pet happiness
+      pikachu_info['happiness'] += 1;
+      // Decrease pet weight
+      pikachu_info['weight'] -= 1;
+      switchAnimation(animations.jump)
+      checkAndUpdatePetInfoInHtml();
+    }
+    
+    function clickedExerciseButton() {
+      // Decrease pet happiness
+      pikachu_info['happiness'] -= 1;
+      // Decrease pet weight
+      pikachu_info['weight'] -= 1;
+      switchAnimation(animations.idle)
+      checkAndUpdatePetInfoInHtml();
+    }
+  
+    function checkAndUpdatePetInfoInHtml() {
+      checkWeightAndHappinessBeforeUpdating();  
+      updatePetInfoInHtml();
+    }
+    
+   function checkWeightAndHappinessBeforeUpdating() {
+
+    if (pikachu_info['weight'] < 0) pikachu_info['weight'] = 0;
+    if (pikachu_info['happiness'] < 0) pikachu_info['happiness'] = 0;
+    if (pikachu_info['happiness'] == 10) switchAnimation(animations.dance);
+}
+    
+    // Updates your HTML with the current values in your pet_info object
+    function updatePetInfoInHtml() {
+      $('.name').text(pikachu_info['name']);
+      $('.weight').text(pikachu_info['weight']);
+      $('.happiness').text(pikachu_info['happiness']);
+    }
+  
